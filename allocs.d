ec3979284813d6ed58592e871b44f9960d6a96cf
Matt Valentine-House <matt@eightbitraptor.com>
2020-09-23 16:19:41 +0100

    fix bug in allocs

--- allocs.d
+++ allocs.d
@@ -1,14 +1,22 @@
 #pragma D option quiet
 
 pid$target::malloc:entry,
-pid$target::calloc:entry,
-pid$target::realloc:entry,
-pid$target::reallocf:entry,
 pid$target::valloc:entry
- {
+{
   printf("probe: %s\n", probefunc);
   printf("TIME: %i\n", timestamp);
   printf("REQUESTED_BYTES: %i", arg0);
+  ustack();
+  printf("\n");
+}
+
+pid$target::calloc:entry,
+pid$target::realloc:entry,
+pid$target::reallocf:entry
+ {
+  printf("probe: %s\n", probefunc);
+  printf("TIME: %i\n", timestamp);
+  printf("REQUESTED_BYTES: %i", arg1);
   ustack();
   printf("\n");
 }
